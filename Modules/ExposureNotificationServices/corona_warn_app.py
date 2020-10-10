b3a24ec45cd995790ff6d46c319677e769e34c70
Pedro José Pereira Vieito <pvieito@gmail.com>
2020-10-10 04:14:18 +0200

    [ExposureNotification] Added support for multiple sources with ExposureNotificationServiceKeysDownloader

--- Modules/ExposureNotificationServices/corona_warn_app.py
+++ Modules/ExposureNotificationServices/corona_warn_app.py
@@ -12,25 +12,28 @@ _corona_warn_app_default_country = "DE"
 
 class CoronaWarnAppServiceKeysDownloader(ExposureNotificationServiceKeysDownloader):
     def generate_exposure_keys_export_endpoints_with_parameters(
-            self, server_endpoint_url: str = None, country: str = None, **kwargs) -> List[dict]:
+            self, server_endpoint_url: str = None, countries: List[str] = None, **kwargs) -> List[dict]:
         if server_endpoint_url is None:
             server_endpoint_url = _corona_warn_app_default_server_endpoint_url
-        if country is None:
-            country = _corona_warn_app_default_country
+        if countries is None:
+            countries = _corona_warn_app_default_country
+        elif isinstance(countries, str):
+            countries = [countries]
 
         exposure_keys_export_endpoints = []
         dates = self.get_dates_from_parameters(**kwargs)
         for date in dates:
-            upload_date_string = date.strftime("%Y-%m-%d")
-            diagnosis_keys_endpoint_url = \
-                server_endpoint_url + \
-                _corona_warn_app_server_diagnosis_keys_endpoint_path
-            exposure_keys_export_endpoint = diagnosis_keys_endpoint_url.format(
-                country=country, upload_date_string=upload_date_string)
-            exposure_keys_export_endpoints.append(dict(
-                endpoint=exposure_keys_export_endpoint,
-                country=country,
-                upload_date=upload_date_string,
-                server_endpoint_url=server_endpoint_url,
-            ))
+            for country in countries:
+                upload_date_string = date.strftime("%Y-%m-%d")
+                diagnosis_keys_endpoint_url = \
+                    server_endpoint_url + \
+                    _corona_warn_app_server_diagnosis_keys_endpoint_path
+                exposure_keys_export_endpoint = diagnosis_keys_endpoint_url.format(
+                    country=country, upload_date_string=upload_date_string)
+                exposure_keys_export_endpoints.append(dict(
+                    endpoint=exposure_keys_export_endpoint,
+                    country=country,
+                    upload_date=upload_date_string,
+                    server_endpoint_url=server_endpoint_url,
+                ))
         return exposure_keys_export_endpoints
