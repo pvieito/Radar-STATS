9ae1d2877df0e253518e9d504e984abf8dfbbb2f
Pedro José Pereira Vieito <pvieito@gmail.com>
2020-10-18 22:24:47 +0200

    [ExposureNotification] Better Corona-Warn-App backend implementation

--- Modules/ExposureNotification/Backends/corona_warn_app.py
+++ Modules/ExposureNotification/Backends/corona_warn_app.py
@@ -3,7 +3,7 @@ from typing import *
 from .base import BaseBackendKeysDownloader
 
 _corona_warn_app_server_diagnosis_keys_endpoint_path = \
-    "/version/v1/diagnosis-keys/country/{country}/date/{upload_date_string}"
+    "/version/v1/diagnosis-keys/country/{country}/date"
 
 
 class CoronaWarnAppBackendKeysDownloader(BaseBackendKeysDownloader):
@@ -13,22 +13,26 @@ class CoronaWarnAppBackendKeysDownloader(BaseBackendKeysDownloader):
 
     def generate_exposure_keys_export_endpoints_with_parameters(
             self, **kwargs) -> List[dict]:
+        diagnosis_keys_endpoint_url = \
+            self.server_endpoint_url + \
+            _corona_warn_app_server_diagnosis_keys_endpoint_path.format(
+                country=self.target_country)
+
+        response = self.send_get_request(diagnosis_keys_endpoint_url)
+        response.raise_for_status()
+        upload_dates = response.json()
+
         exposure_keys_export_endpoints = []
-        dates = self.get_dates_from_parameters(**kwargs)
-        for date in dates:
-            upload_date_string = date.strftime("%Y-%m-%d")
-            diagnosis_keys_endpoint_url = \
-                self.server_endpoint_url + \
-                _corona_warn_app_server_diagnosis_keys_endpoint_path
-            exposure_keys_export_endpoint = diagnosis_keys_endpoint_url.format(
-                country=self.target_country, upload_date_string=upload_date_string)
+        for upload_date in upload_dates:
+            exposure_keys_export_endpoint = \
+                diagnosis_keys_endpoint_url + "/" + upload_date
             exposure_keys_export_endpoints.append(dict(
                 endpoint=exposure_keys_export_endpoint,
                 country=self.target_country,
-                upload_date=upload_date_string,
+                upload_date=upload_date,
                 endpoint_identifier_components=[
                     self.target_country,
-                    upload_date_string
+                    upload_date
                 ],
             ))
         return exposure_keys_export_endpoints
